a3e6d5993febf009ba3905115eb80a11490c617e
Michele Schulz <michelecaleb.schulz@gmail.com>
2022-12-17 21:07:43 +0000

    Add logic to fetchISSFlyOverTimes to pass tests in index.js

--- iss.js
+++ iss.js
@@ -54,4 +54,30 @@ const fetchCoordByIP = function(ip, callback) {
   });
 };
 
-module.exports = { fetchMyIP, fetchCoordByIP };
+/**
+ * Makes a single API request to retrieve upcoming ISS fly over times the for the given lat/lng coordinates.
+ * Input:
+ *   - An object with keys `latitude` and `longitude`
+ *   - A callback (to pass back an error or the array of resulting data)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The fly over times as an array of objects (null if error). Example:
+ *     [ { risetime: 134564234, duration: 600 }, ... ]
+ */
+const fetchISSFlyOverTimes = function(coords, callback) {
+  request(`https://iss-flyover.herokuapp.com/json/?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching fly over times. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+    const bodyObj = JSON.parse(body);
+    callback(null, bodyObj.response);
+  });
+};
+
+module.exports = { fetchMyIP, fetchCoordByIP, fetchISSFlyOverTimes };
